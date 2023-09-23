99472f0223c5171ba87609d026ebfcd0e2c0db14
dilip coder <mihirsingh241@gmail.com>
2023-09-23 12:51:12 +0530

    change backround and apply some responsiveness

--- app/page.jsx
+++ app/page.jsx
@@ -32,13 +32,13 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex justify-center items-center w-screen h-screen bg-no-repeat bg-cover bg-center bg-[url('https://t3.ftcdn.net/jpg/04/34/58/54/360_F_434585463_zpdtTpTEbqQFfsp6RVEW6IIxEM9dHf86.jpg')]">
-        <div className="h-3/5 w-2/5  rounded backdrop-blur-3xl p-8 space-y-4 relative flex justify-center item-center flex-col">
+      <div className="flex justify-center items-center w-full h-screen bg-no-repeat bg-cover bg-center bg-[url('https://img.freepik.com/premium-vector/global-currency-background_115579-800.jpg')]">
+        <div className="h-3/5 xl:w-2/5 w-11/12 rounded backdrop-blur-3xl p-8 space-y-4 relative flex justify-center item-center flex-col">
 
           <Whitebox state={"From"} value={from} setter={setfrom} setcon={setfcon} countries={countries} con={fcon} />
           <Whitebox state={"To"} value={to} setter={setto} setcon={settcon} countries={countries} con={tcon} disable />
 
-          <button onClick={converter} className="text-white w-full bg-blue-600 rounded py-3">Convert <span className="uppercase">{fcon}</span> To <span className="uppercase">{tcon}</span></button>
+          <button onClick={converter} className="text-white w-full bg-blue-600 rounded py-3 xl:text-lg text-xs">Convert <span className="uppercase">{fcon}</span> To <span className="uppercase">{tcon}</span></button>
         </div>
       </div>
     </>
--- app/whitebox.jsx
+++ app/whitebox.jsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react";
 
 
 
@@ -9,15 +8,15 @@ function whitebox(props) {
 
 
   return (
-    <div className="h-1/3 bg-white rounded-lg p-4 flex justify-between items-center relative">
+    <div className="h-1/3 bg-white rounded-lg p-4 flex  justify-between items-center">
 
       <div className="space-y-2">
-        <h1 className="text-lg">{props.state}</h1>
-        <input type="number" value={props.value} onChange={(e) => { props.setter(e.target.value) }} disabled={props.disable} className="px-1 font-bold text-lg rounded outline-none border-2 border-slate-300" placeholder={0} />
+        <h1 className="xl:text-lg text-sm">{props.state}</h1>
+        <input type="number" value={props.value} onChange={(e) => { props.setter(e.target.value) }} disabled={props.disable} className="px-1 font-bold sm:text-lg text-sm rounded outline-none border-2 border-slate-300 w-1/2 sm:w-auto" placeholder={0} />
       </div>
 
       <div className="space-y-2 flex flex-col">
-        <h1>Currency Type</h1>
+        <h1 className="text-xs md:text-lg">Currency Type</h1>
         <select id="fcur" className="justify-items-end border-2 border-slate-200 bg-slate-100 rounded" onChange={(e) => props.setcon(e.target.value)}>
           <option value={`${props.con}`}>{props.con}</option>
           {props.countries.map((elem, idx) => (
